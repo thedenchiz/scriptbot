019d552c53cce922acd4f2d8a43bb530f0e10ee9
denchiz <densur1999@gmail.com>
2026-06-21 14:13:58 +0300

    Add configuration variables for bot and database

---
+++ config.py
@@ -0,0 +1,11 @@
+# Сюда вставишь токены когда будешь готов
+BOT_TOKEN = "ВВЕДИ_ТОКЕН_БОТА"
+GEMINI_API_KEY = "AIzaSyAfpRDeGodMcNxXq7g-Wk1lzkyycBtzTLQ"
+
+ADMIN_ID = 695915869
+
+# Название скрипта для генерации ключа
+SCRIPT_NAME = "thefy"
+
+# Файл базы данных
+DB_FILE = "db.json"
